54c01f7dd5967f95a0a490b8059a3501b34b517d
Ömer Alkan <120298619+1omeralkan@users.noreply.github.com>
2026-03-04 16:13:50 +0300

    Proje iskeleti olusturuldu ve jasypt ile veritabani baglantisi saglandi

---
+++ src/main/java/com/omeralkan/parameter/ParameterMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omeralkan.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParameterMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omeralkan/parameter/ParameterMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeralkan.parameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
